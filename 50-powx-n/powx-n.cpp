39e90e9b740829c79cbd03695ead190cbe2a974e
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-08-04 20:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.51%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double pospower(double x, long long n) {
+        if (n == 0) return 1;
+        double y = pospower(x, n / 2);
+        if (n % 2 == 0) {
+            return y * y;
+        } else {
+            return x * y * y;
+        }
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;  // To handle INT_MIN safely
+        if (N < 0) {
+            N = -N;
+            return 1.0 / pospower(x, N);
+        } else {
+            return pospower(x, N);
+        }
+    }
+};
